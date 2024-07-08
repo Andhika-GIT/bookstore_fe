403e4495842dc000a0032d25e271aa02232398b2
Andhika-GIT <andhikapr081@gmail.com>
2024-07-08 13:23:27 +0700

    feat: temp error handling

--- app/actions/book.ts
+++ app/actions/book.ts
@@ -5,13 +5,17 @@ import { Book } from "@/types";
 const BASE_URL = process.env.BASE_URL;
 
 export const getBooks = async (page: number = 1): Promise<Book[] | undefined> => {
-  const response = await fetch(`${BASE_URL}/book?page=${page}`, {
-    cache: "no-cache",
-  });
+  try {
+    const response = await fetch(`${BASE_URL}/book?page=${page}`, {
+      cache: "no-cache",
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return data;
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const getOneBook = async (id: number): Promise<Book | undefined> => {
